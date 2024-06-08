30a3c8a47916de366fb9fdbee188caada802126a
hirusha-adi <hirushaadi@gmail.com>
2024-06-08 10:57:19 +0530

    fix disastrous old code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+
 output = []
 for root, dirs, files in os.walk("."):
-    for filename in files:
-        if '.txt' in filename:
-            output.append(open(filename, 'r', encoding='utf-8',
-                          errors='ignore').read()+'\n')
-with open('output.txt', 'w', encoding='utf-8', errors='ignore') as p:
-    p.writelines(output)
+    for filename in [f for f in files if f.endswith('.txt')]: # txt files only
+        filepath = os.path.join(root, filename)
+        with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
+            output.append(file.read() + '\n')
+
+with open('output.txt', 'w', encoding='utf-8', errors='ignore') as output_file:
+    output_file.writelines(output)
